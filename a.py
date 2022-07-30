ea27ca05736f2d1a19469f21e37f90d99c8666e2
HashimotoAyato <ayaenum@gmail.com>
2022-07-30 23:22:42 +0900

    make change

--- a.py
+++ a.py
@@ -1,21 +1,29 @@
 N = int(input())
 S = list(input())
 
-if S[0] == S[-1]:
-    check = True
+if S[0] == S[N-1]:
+    pass
 else:
     if S[0] == 'B':
-        S[-1] = 'B'
-        S[-2] = 'A'
-        check = True
+        S[0] = 'A'
+        S[1] = 'B'
     else:
-        check = False
+        pass
 
 for i in range(1, int(N/2)):
     if S[i] == S[N-i-1]:
-        
+        continue
+    else:
+        if S[i] == 'B':
+            S[i] = 'A'
+            S[i+1] = 'B'
+        else:
+            if S[N-i] == 'B':
+                S[N-i-1] = 'A'
+            elif S[i-1] == 'A':
+                S[i] = 'B'
 
-
+check = True
 for i in range(int(N/2)):
     if S[i] == S[N-i-1]:
         pass
